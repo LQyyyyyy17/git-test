d5239bb8d94369ce3e418c31ea25ebdb5d330b44
LQyyyyyy17 <l15004253406@163.com>
2023-03-10 08:32:26 +0800

    second commit

---
+++ src/main/java/com/guigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.guigu.git;
+
+/**
+ * @Author 吕千映
+ * @Date 2023/3/10日 8:31
+ * @Email l15004253406@163.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

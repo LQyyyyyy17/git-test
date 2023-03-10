57e2191178688e305805c5113ad2d9493b6ca7bc
LQyyyyyy17 <l15004253406@163.com>
2023-03-10 08:54:01 +0800

    hot_fix commit1

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -8,5 +8,7 @@ package com.guigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
